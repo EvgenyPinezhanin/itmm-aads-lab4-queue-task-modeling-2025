f455faa943ed9fbdc8fda075973535a2a4a787d0
EvgenyPinezhanin <pinezhanin@yandex.ru>
2025-11-26 00:35:32 +0300

    Added patterns for tests

---
+++ test/test_tcomputecentermodel.cpp
@@ -0,0 +1,8 @@
+#include "tcomputecentermodel.h"
+
+#include <gtest.h>
+
+TEST(TComputeCenterModel, test)
+{
+  ADD_FAILURE();
+}
---
+++ test/test_tqueue.cpp
@@ -0,0 +1,8 @@
+#include "tqueue.h"
+
+#include <gtest.h>
+
+TEST(TQueue, test)
+{
+  ADD_FAILURE();
+}
